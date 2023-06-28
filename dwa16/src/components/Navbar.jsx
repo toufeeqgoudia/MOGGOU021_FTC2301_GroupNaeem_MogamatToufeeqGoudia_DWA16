96f942c0a0272e3f507f30515637ade5c2c61bc7
toufeeqgoudia <125878241+toufeeqgoudia@users.noreply.github.com>
2023-06-28 19:22:10 +0200

    completed DWA16

---
+++ dwa16/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import airbnb from '../images/airbnb-logo.png'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={airbnb} className="nav--logo" />
+        </nav>
+    )
+}
